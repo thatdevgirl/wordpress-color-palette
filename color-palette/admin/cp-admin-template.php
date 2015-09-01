cc1be757a2ad1c566b2ed9ea47d2b49fc042ad4d
Joni Halabi <joni@jhalabi.com>
2015-09-01 14:05:40 -0400

    Admin: Tweaking instructions to add info about short codes.

--- color-palette/admin/cp-admin-template.php
+++ color-palette/admin/cp-admin-template.php
@@ -2,18 +2,18 @@
 
 <h1 class="cp__h1">Color Palette Settings</h1>
 
-<p>Use this page to set up the primary colors of your web site's style guide.</p>
+<p>Use this page to set up the color palettes for your web site's style guide.</p>
 
 <form action="" method="post">
     <input type="hidden" name="hasUpdates" value="Y" />
 
     <h2 class="cp__h2">Primary Colors</h2>
-    <p>These are the primary colors used on your site and in your brand.</p>
+    <p>These are the primary colors used on your site and in your brand. Use the shortcode <strong>[primarycolors]</strong> to display this palette on your pages.</p>
 
     <?php insertColorRow($primaryColors, 'cp-primary', 'primaryColors'); ?>
 
     <h2 class="cp__h2">Accent Colors</h2>
-    <p>These are the accent colors used on your site and in your brand that compliment your primary colors.</p>
+    <p>These are the accent colors used on your site and in your brand that compliment your primary colors. Use the shortcode <strong>[accentcolors]</strong> to display this palette on your pages.</p>
     
     <?php insertColorRow($accentColors, 'cp-accent', 'accentColors'); ?>
 
