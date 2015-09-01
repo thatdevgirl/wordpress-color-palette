ae00ec23376d5332fb4b3256f1ed53a1cca8bce2
Joni Halabi <joni@jhalabi.com>
2015-09-01 12:04:27 -0400

    Fixing the tint display that I broke with an earlier commit.

--- color-palette/frontend/cp-accent.php
+++ color-palette/frontend/cp-accent.php
@@ -6,16 +6,19 @@
         <?php $cmyk = implode(', ', hex2cmyk($color)); ?>
 
         <div class="tints" style="background: #<?php echo $color; ?>">
-             <div class="tints__div">
+             <header class="tints__header">
                 <p class="tints__p">#<?php echo $color; ?></p>
                 <p class="tints__p">RGB <?php echo $rgb; ?></p>
                 <p class="tints__p">CMYK <?php echo $cmyk; ?></p>
-            </div>
+            </header>
             
+            <div class="tints__div">
                 <?php for($i=20; $i<=80; $i=$i+20): ?>
-                    <p class="tints__p--tint"><?php echo $i; ?>%</p>
+                    <p class="tints__p--tint" style="background: rgba(<?php echo $rgb . ', ' . $i/100; ?>)">
+                        <?php echo $i; ?>%
+                    </p>
                 <?php endfor; ?>
-            </table>
+            </div>
         </div>
     <?php endforeach; ?>
 </section>
