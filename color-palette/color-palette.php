64c8b801cc96e5a3275e3f4445cc4c56d82e1e93
Joni Halabi <jonihalabi@UIS-AL-8CG8WM.local>
2017-11-16 12:57:49 -0500

    Adding icons; updating author uri

--- color-palette/color-palette.php
+++ color-palette/color-palette.php
@@ -8,7 +8,7 @@ Plugin Name: Color Palette
 Description: This WordPress plugin creates a custom color palette section that you can add to any page (i.e. in a style guide).
 Version: 2.0
 Author: Joni Halabi
-Author URI: http://www.jhalabi.com
+Author URI: http://www.thatdevgirl.com
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
